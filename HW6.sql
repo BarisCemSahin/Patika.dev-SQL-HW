426b369811dcce1cf96800074913607904df2d1c
BarisCemSahin <100863772+BarisCemSahin@users.noreply.github.com>
2022-03-19 00:21:50 +0300

    Update HW6.sql

--- HW6.sql
+++ HW6.sql
@@ -13,4 +13,4 @@ WHERE rental_rate = 0.99
 -- 4. film tablosunda bulunan filmlerin uzunluğu 150 dakikadan büyük olanlarına ait kaç farklı replacement_cost değeri vardır?
 SELECT COUNT(DISTINCT replacement_cost) FROM film
 WHERE length > 150;
-BETWEEN IN SELECT
+BETWEEN
